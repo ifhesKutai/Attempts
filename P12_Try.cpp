1041d868fe40ce39296c92764b7309dcad6f3c65
Ifhes <32117554+ifhesKutai@users.noreply.github.com>
2019-05-14 20:16:00 -0500

    Create P12_Try.cpp

---
+++ P12_Try.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+int* getPrimeFactors(int num){
+  int auxPrim = 2;
+  int aux = num;
+  int * Primes;
+  Primes = new int [500]
+  int compare1;
+  float compare2;
+  while(aux > 1){
+    while(1){
+      compare1 = aux/auxPrim;
+      compare2 = (aux*1.0)/auxPrim;
+      if(compare1 == compare2){
+        
+      }
+    }
+  }
+}
+
+int getNumberofFactors(int* primes){
+  
+}
+
+int main(){
+  
+}
